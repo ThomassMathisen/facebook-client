9f2bc7d4e38d6d172bbdbef12da35c31b6b6428b
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-11 16:43:32 +0100

    Create register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,6 +2,27 @@ import "./register.css"
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="login">
+      <div className="loginwrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">facebook</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Facebook
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Username" className="loginInput" />
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <input placeholder="Confirm Password" className="loginInput" />
+            <button className="loginButton">Sign up</button>
+            <button className="loginRegisterButton">
+              Log in
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
