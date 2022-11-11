7b731b15d61f14f2b9d336a5f293c222b2d7c5ad
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-11 16:30:33 +0100

    Setup two rightbar scenarios

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,10 @@ import "./rightbar.css"
 import { Users } from "../../dummyData"
 import Online from "../online/Online"
 
-const Rightbar = () => {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+export default function Rightbar({profile}) {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
           <img className="birthdayImg" alt="" src="assets/gift.png" />
           <span className="birthdayText">
@@ -19,9 +19,87 @@ const Rightbar = () => {
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    )
+  }
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Philadelphia</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">It' Complicated</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">Friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img 
+              className="rightbarFollowingImg"
+              alt=""
+              src="assets/person/2.jpeg"
+            />
+            <span className="rightbarFollowingName">John Ayers</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img 
+              className="rightbarFollowingImg"
+              alt=""
+              src="assets/person/1.jpeg"
+            />
+            <span className="rightbarFollowingName">Jane Ayers</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img 
+              className="rightbarFollowingImg"
+              alt=""
+              src="assets/person/5.jpeg"
+            />
+            <span className="rightbarFollowingName">Rita Hayworth</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img 
+              className="rightbarFollowingImg"
+              alt=""
+              src="assets/person/8.jpeg"
+            />
+            <span className="rightbarFollowingName">Frank Sinatra</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img 
+              className="rightbarFollowingImg"
+              alt=""
+              src="assets/person/10.jpeg"
+            />
+            <span className="rightbarFollowingName">Chucky Cheese</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img 
+              className="rightbarFollowingImg"
+              alt=""
+              src="assets/person/9.jpeg"
+            />
+            <span className="rightbarFollowingName">Gal Gadot</span>
+          </div>
+        </div>
+      </>
+    )
+  }
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   )
 }
-
-export default Rightbar
