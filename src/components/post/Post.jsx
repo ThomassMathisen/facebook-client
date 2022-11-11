63e12b3f05bc0186c364cc159aea6e74a49aa3b1
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-11 14:48:19 +0100

    Create Rightbar component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -36,6 +36,99 @@ const Post = () => {
             <span className="postShareText">5 Shares</span>
           </div>
         </div>
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img 
+              className="postProfileImg"
+              src="assets/person/5.jpeg"
+              alt=""
+            />
+            <span className="postUsername">
+                Jack Doe
+            </span>
+            <span className="postDate">10 minutes ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">This is a post</span>
+          <img className="postImg" alt="" src="/assets/post/1.jpeg" />
+        </div>
+        <div className="postBottom">
+          <div className="bottomLeft">
+            <img className="likeIcon" alt="" src="assets/like.png" />
+            <img className="likeIcon" alt="" src="assets/heart.png" />
+            <span className="postLikeCounter">10 Likes</span>
+          </div>
+          <div className="bottomRight">
+            <span className="postCommentText">5 Comments</span>
+            <span className="postShareText">5 Shares</span>
+          </div>
+        </div>
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img 
+              className="postProfileImg"
+              src="assets/person/5.jpeg"
+              alt=""
+            />
+            <span className="postUsername">
+                Jack Doe
+            </span>
+            <span className="postDate">10 minutes ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">This is a post</span>
+          <img className="postImg" alt="" src="/assets/post/1.jpeg" />
+        </div>
+        <div className="postBottom">
+          <div className="bottomLeft">
+            <img className="likeIcon" alt="" src="assets/like.png" />
+            <img className="likeIcon" alt="" src="assets/heart.png" />
+            <span className="postLikeCounter">10 Likes</span>
+          </div>
+          <div className="bottomRight">
+            <span className="postCommentText">5 Comments</span>
+            <span className="postShareText">5 Shares</span>
+          </div>
+        </div>
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img 
+              className="postProfileImg"
+              src="assets/person/5.jpeg"
+              alt=""
+            />
+            <span className="postUsername">
+                Jack Doe
+            </span>
+            <span className="postDate">10 minutes ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">This is a post</span>
+          <img className="postImg" alt="" src="/assets/post/1.jpeg" />
+        </div>
+        <div className="postBottom">
+          <div className="bottomLeft">
+            <img className="likeIcon" alt="" src="assets/like.png" />
+            <img className="likeIcon" alt="" src="assets/heart.png" />
+            <span className="postLikeCounter">10 Likes</span>
+          </div>
+          <div className="bottomRight">
+            <span className="postCommentText">5 Comments</span>
+            <span className="postShareText">5 Shares</span>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,60 @@ import "./rightbar.css"
 const Rightbar = () => {
   return (
     <div className="rightbar">
-      Rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" alt="" src="assets/gift.png" />
+          <span className="birthdayText">
+            <b>Jane Fonda</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img className="rightbarAd" alt="" src="assets/ad.png" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/6.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jack Johnson</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/6.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jack Johnson</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/6.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jack Johnson</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/6.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jack Johnson</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/6.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jack Johnson</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/6.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jack Johnson</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
